e5ef016a1d0f20158a250fc2c7f784a8676122be
gbr-sj <46060864+satoshi-2000@users.noreply.github.com>
2022-04-25 17:01:25 +0900

    Create spearmanr.py

---
+++ spearmanr.py
@@ -0,0 +1,14 @@
+# This code is besed on this article.
+# https://qiita.com/dacciinfo/items/88debe69f9f4e927aafc
+
+from scipy.stats import spearmanr
+
+xdata = [1,2,1,2,3,4,2,5]
+ydata = [2,1,1,2,3,4,3,5]
+
+correlation, pvalue = spearmanr(xdata, ydata)
+print(correlation) #0.6727272727272726
+"""
+0.83756543735604
+0.009451676093682563
+"""
