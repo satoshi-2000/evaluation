6920c517e40891226919d80b5635352cab2e43d3
gbr-sj <46060864+satoshi-2000@users.noreply.github.com>
2022-04-25 17:10:39 +0900

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+xdata = [1,2,1,2,3,4,2,5]
+ydata = [2,1,1,2,3,4,3,5]
+zdata = [2,4,1,1,3,4,2,4]
+
+# 散布図を作成 (cで色を設定)
+plt.scatter(xdata,ydata,c="red")
+plt.scatter(xdata,zdata,c="blue")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
