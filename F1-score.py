c1fcb41920ce974f150ad626d174181de43bcc79
gbr-sj <46060864+satoshi-2000@users.noreply.github.com>
2022-04-25 16:57:56 +0900

    Update F1-score.py

--- F1-score.py
+++ F1-score.py
@@ -43,3 +43,22 @@ F1score
 0.6666666666666666
 1 4 1 0
 """
+
+# sklearnを用いた計算方法
+
+from sklearn.metrics import classification_report
+
+print(classification_report(xdata, ydata))
+"""
+        precision    recall  f1-score   support
+
+           1       0.50      0.50      0.50         2
+           2       0.50      0.33      0.40         3
+           3       0.50      1.00      0.67         1
+           4       1.00      1.00      1.00         1
+           5       1.00      1.00      1.00         1
+
+    accuracy                           0.62         8
+   macro avg       0.70      0.77      0.71         8
+weighted avg       0.62      0.62      0.61         8
+"""
